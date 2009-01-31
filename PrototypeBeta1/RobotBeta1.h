7424ef35b9838a6875fd7b8f8c34752bc47ef935
loafdog <loafdog@0d94b9bc-da79-11dd-9e03-45b345501c35>
2009-01-31 20:09:50 +0000

    Maciej: Add dashboard code. Change member vars to be pointers instead of references.

---
+++ PrototypeBeta1/RobotBeta1.h
@@ -0,0 +1,28 @@
+#ifndef _ROBOTBETA1_H
+#define _ROBOTBETA1_H
+
+class RobotBeta1 : public SimpleRobot {
+public:
+		RobotBeta1(void);
+		void Autonomous(void);
+		void OperatorControl(void);
+		void RobotMain(void);
+		void UpdateDashboard(void);
+		
+private:
+		RobotDrive *itsDrive;
+		Joystick *joystickUSB1;
+		Joystick *joystickUSB2;
+		Gyro *gyro;
+		PCVideoServer *pc;
+		DashboardDataFormat *dashboardDataFormat;
+		
+		void driveStrait(long maxTime);
+		void turn90Right();
+		void turn130Left(); 
+		void resetGyro();
+		void TestCamera();
+		
+};
+
+#endif
