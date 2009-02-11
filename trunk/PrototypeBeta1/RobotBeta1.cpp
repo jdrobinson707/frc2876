1e55db848ca795072e6fb14bf88ea6bd8d6161bc
loafdog <loafdog@0d94b9bc-da79-11dd-9e03-45b345501c35>
2009-02-11 03:07:05 +0000

    Maciej & Eddy: Add new joystick. Change button handling a bit. Move button code into new functions. Add driver station alliance check.

--- trunk/PrototypeBeta1/RobotBeta1.cpp
+++ trunk/PrototypeBeta1/RobotBeta1.cpp
@@ -44,6 +44,7 @@ using namespace std;
 
 #define JOYSTICK_LEFT 1
 #define JOYSTICK_RIGHT 2
+#define JOYSTICK_COPILOT 3
 
 // Which pwm inputs/outputs the drive motors are plugged into.
 #define DRIVE_MOTOR_LEFT_PWM 1
@@ -63,7 +64,8 @@ using namespace std;
 
 int dbg_flag = 1;
 #define DBG if (dbg_flag)printf 
-
+#define TRACE_ENTER printf("%s Enter\n", __func__);
+#define TRACE_EXIT printf("%s Exit\n", __func__);
 
 RobotBeta1::RobotBeta1(void)
 {
@@ -75,18 +77,20 @@ RobotBeta1::RobotBeta1(void)
 	robotDrive = new RobotDrive(leftMotor, rightMotor);
 	stickLeft = new Joystick(JOYSTICK_LEFT);
 	stickRight = new Joystick(JOYSTICK_RIGHT);
+	stickCopilot = new Joystick(JOYSTICK_COPILOT);
 	gyro = new Gyro(ANALOG_MODULE_SLOT, GYRO_ANGLE_CHANNEL);
 	shooter = new Jaguar(DIGITAL_MODULE_SLOT, SHOOTER_MOTOR_PWM);
 	conveyor = new Jaguar(DIGITAL_MODULE_SLOT, CONVEYOR_MOTOR_PWM);
 	dashboard = new DashboardDataFormat();
 	pan = new Servo(DIGITAL_MODULE_SLOT, 10);
 	tilt = new Servo(DIGITAL_MODULE_SLOT, 9);
-	
-	
+	//encoder = new Encoder(DIGITAL_MODULE_SLOT, 1);
+	driverStation = DriverStation::GetInstance();
+	// initializeAlliance();
 	initializeColors();
 	initializeButtons();
 	initializeCamera();
-	
+	lastTime = time(NULL);
 	GetWatchdog().SetExpiration(WATCHDOG_EXPIRATION);
 	
 	DBG("Done\n");
@@ -108,9 +112,28 @@ RobotBeta1::~RobotBeta1(void)
 	delete conveyor;
 	delete pan;
 	delete tilt;
+	// delete encoder;
+	delete stickCopilot;
+}
+
+void RobotBeta1::initializeAlliance() {
+	TRACE_ENTER;
+	
+	// TODO add code to pick alliance based on physical switch setting
+	
+	DriverStation::Alliance alliance = driverStation->GetAlliance();
+	if (alliance == DriverStation::kRed) {
+		DBG("RED TEAM\n");
+	} else if (alliance == DriverStation::kBlue) {
+		DBG("BLUE TEAM\n");
+	} else {
+		DBG("Invalid team color");
+	}
+	TRACE_EXIT;
 }
 
 void RobotBeta1::initializeColors() {
+	TRACE_ENTER;
 	/* image data for tracking - override default parameters if needed */
 	/* recommend making PINK the first color because GREEN is more 
 	 * subsceptible to hue variations due to lighting type so may
@@ -130,74 +153,48 @@ void RobotBeta1::initializeColors() {
 	tt2.saturation.minValue = 58;   
 	tt2.saturation.maxValue = 255;    
 	tt2.luminance.minValue = 92;  
-	tt2.luminance.maxValue = 255;	
+	tt2.luminance.maxValue = 255;
+	DBG("Exit\n");
 }
 
 void RobotBeta1::initializeButtons(void)
 {
+	TRACE_ENTER;
 	int i;
 	for (i = JOYSTICK_FIRST_BUTTON; i <= JOYSTICK_NUM_BUTTONS; i++) {
 		leftButtons[i] = rightButtons[i] = false;
 	}
+	TRACE_EXIT;
 }
 
 void RobotBeta1::initializeCamera(void)
 {
-	if (StartCameraTask(10, 0, k320x240, ROT_180) == -1) { 
+	TRACE_ENTER;
+	if (StartCameraTask(10, 0, k160x120, ROT_180) == -1) { 
 		DBG("Failed to spawn camera task; Error code %s\n", 
 				GetVisionErrorText(GetLastVisionError()) ); 
 	} else {
 		pc = new PCVideoServer();
-	} 	
+	}
+	TRACE_EXIT;
 }
 
-static int fake_range = 10;
-static int getRange()
+void RobotBeta1::Autonomous(void) 
 {
-	return fake_range;
-}
-
-
-void RobotBeta1::Autonomous(void) {
 	DBG("Starting Autonomous...Trial 78\n");
 	//conveyor->Set(.5);
-	
 	while(IsAutonomous()) {
-		#if 0
+#if 0
 		GetWatchdog().Feed();
 		driveStrait(500);
 		GetWatchdog().Feed();
-		turn90Right();
-		robotDrive->Drive(0, 0);
-		#endif
-		 
-		#if 0
-		if (getRange() == 5) {
-			shooter->Set(1);
-		} else if  (getRange() == 4) {
-			shooter->Set(.75);
-		}	else if (getRange() == 3) {
-			shooter->Set(.75);
-		}	else if (getRange() ==2) {
-			shooter->Set(.5);
-		} else {
-			shooter->Set(0);
-		}
+		turn130Left();
+		robotDrive.Drive(0, 0);
+#endif
+
 		UpdateDashboard();
-		if (fake_range == 0) {
-					fake_range = 10;
-				} else {
-					fake_range = fake_range - 1;
-				}
-		#endif
-		#if 1
-		recieveAndReactToCameraData();
 		GetWatchdog().Feed();
-		Wait(.05);
-		GetWatchdog().Feed();
-		#endif
-	}
-	robotDrive->Drive(0, 0);
+	} 
 	conveyor->Set(0);
 	DBG("\nEnd Autonomous Mode\n");	
 }
@@ -206,7 +203,7 @@ void RobotBeta1::OperatorControl(void) {
 	DBG("\nStart Operator Control...\n");
 	
 	resetGyro();
-	conveyor->Set(.5);
+	// conveyor->Set(.5);
 	
 	GetWatchdog().SetEnabled(true);
 	while (IsOperatorControl())  {
@@ -220,7 +217,7 @@ void RobotBeta1::OperatorControl(void) {
 	DBG("\nEnd Operator Control\n");
 }
 /************************************************************
-NOTE:  FindTwoColors (located in Target.cpp) takes its own raw images so if
+NOTE:  FindTwoColors takes its own raw images so if
 	   the camera is upside down then FindTwoColors will
 	   see that pink is above even though it really is below
 ************************************************************/
@@ -228,13 +225,9 @@ void RobotBeta1::recieveAndReactToCameraData(void) {
 	if (FindTwoColors(tt1, tt2, ABOVE, &pa1, &pa2)) { //if the trailor's beam is found
 		//STEP#1:	measure distance to trailer's beam
 		double dToTrailor = 0;
-		double heightOfColor = 0;
-		heightOfColor = (double)(pa1.boundingRect.height + pa2.boundingRect.height);
-		dToTrailor = distanceToTrailor(heightOfColor);
-		cout << "\nHeight:  "; cout << (heightOfColor); cout << "\n";
+		dToTrailor = distanceToTrailor((double)(pa1.boundingRect.height + pa2.boundingRect.height));
 		cout << "\nDistance To Trailor:  ";  cout << dToTrailor; cout << "\n";
 		//STEP#2:	align
-		moveToTrailor(dToTrailor);
 		//STEP#3:	shoot
 	} else {
 		cout << "Else------------------";
@@ -261,65 +254,16 @@ void RobotBeta1::moveToTrailor(double distance) {
  * Post:  returns the distance of the trailer from the robot in feet  */
 double RobotBeta1::distanceToTrailor(double pxHeightOfColor) {
 	double d = 0;
+#if 0
+	d = (344.0/(pxHeightOfColor));
+	return d;
+#endif
 	double const constant = 8520.0;
 	d = (constant/(pxHeightOfColor));
 	d = d / 12.0;   //convert inches to feet
 	return d; // in feet
 }
 
-void RobotBeta1::TestCamera(void) {
-	Image *image;
-	double timestamp;
-	DBG("Enter");
-	image = frcCreateImage(IMAQ_IMAGE_RGB);
-	if (image == NULL) {
-		DBG("failed to create image %s\n", GetVisionErrorText(GetLastVisionError()));
-	}
-	if (!GetImage(image, &timestamp)) { 
-		DBG("error: %s", GetVisionErrorText(GetLastVisionError()));
-	}
-}
-
-
-void RobotBeta1::UpdateDashboard(void) 
-{
-	GetWatchdog().Feed();
-
-	// TODO EddyL add dashboard data here.  How?
-	//
-	// Look at dashboard in labview.  
-	// Look at what sensors/motors we have hooked up to cRIO on robot.
-	// Look at the DashboardDataFormat class and see what member variables are.
-	// Make sure the member variable type in DashboardDataFormat  matches the
-	// sensor reading function return type.
-	
-	// Here's an example.  Ok, we don't have anything hooked up to solenoid.
-	// But if we did we would have to get a reading from sensor hooked up to solenoid 
-	// on cRIO.  
-	// Solenoid *s = new Solenoid(1);
-	// dashboard->m_SolenoidChannels = s->Get(); 
-	//
-	// End of TODO section
-	
-	dashboard->m_PWMChannels[0][0] = leftMotor->GetRaw();
-	dashboard->m_PWMChannels[0][1] = rightMotor->GetRaw();
-	dashboard->m_PWMChannels[0][2] = shooter->GetRaw();
-	dashboard->m_PWMChannels[0][3] = conveyor->GetRaw();
-	dashboard->m_PWMChannels[0][8] = pan->GetRaw();
-	dashboard->m_PWMChannels[0][9] = tilt->GetRaw();
-
-	DBG("\r%d %d %d %d %d %d range=%d     ",
-		dashboard->m_PWMChannels[0][0],
-		dashboard->m_PWMChannels[0][1],
-		dashboard->m_PWMChannels[0][2],
-		dashboard->m_PWMChannels[0][3],
-		dashboard->m_PWMChannels[0][8],
-		dashboard->m_PWMChannels[0][9],
-		fake_range);
-	
-	// Call this last to send data to dashboard.
-    dashboard->PackAndSend();
-}
 
 //
 // Read buttons on a joystick to see if any have been pressed.
@@ -334,38 +278,65 @@ void RobotBeta1::readButtons(Joystick *stick, bool *buttons, char *side)
 		buttons[i] = stick->GetRawButton(i);
 		if (buttons[i] == true) {
 			// DBG("%s stick button %d pressed\n", side, i);
-			
 		}
 	}
 }
 
-
-void RobotBeta1::actOnButtons(void)
+void RobotBeta1::updateConveyor()
 {
-	
-	readButtons(stickLeft, leftButtons, "left");
-	readButtons(stickRight, rightButtons, "right");
-	if (leftButtons[1] == true && rightButtons[1] == true) {
+	static float lastSpeed = 0;
+	float speed;
+	if (copilotButtons[7] == true) {
+		speed = conveyor->Get();
+		if (lastSpeed == speed) {
+			speed = speed - .1;
+			conveyor->Set(speed);
+		}
+	}
+	if (copilotButtons[8] == true) {
+			speed = conveyor->Get();
+			if (lastSpeed == speed) {
+				speed = speed + .1;
+				conveyor->Set(speed);
+			}
+	}
+}
+
+void RobotBeta1::updateShooter()
+{	
+	// Check status of copilot joystick buttons
+	if (copilotButtons[1] == true) {
+		if (shooter->Get() == 0) {
+			shooter->Set(.5);
+		} else {
+			shooter->Set(0);
+		}
+	}
+	if (copilotButtons[4] == true) {
+		shooter->Set(.4);
+	}
+	if (copilotButtons[3] == true) {
+		shooter->Set(.6);
+	}
+	if (copilotButtons[5] == true) {
+		shooter->Set(.8);
+	}	
+#if 0	
+	if (copilotButtons[1] == true && rightButtons[1] == true) {
 		shooter->Set(.75);
-	} else if (leftButtons[1] == true && rightButtons[1] == false) {
-		shooter->Set(1);
-	} else if (leftButtons[1] == false && rightButtons[1] == true) {
+	} else if (copilotButtons[1] == true && rightButtons[1] == false) {
+		shooter->Set(.25);
+	} else if (copilotButtons[1] == false && rightButtons[1] == true) {
 		shooter->Set(.5);
 	} else {
 		shooter->Set(0);
 	}
-	if (rightButtons[11] == 1) {
-		conveyor->Set(0);
-	}
-	if (rightButtons[10] == 1) {
-		conveyor->Set(.2);
-	}
-	if (rightButtons[9] == 1) {
-		conveyor->Set(-.2);
-	}
-	// Eddy.  Add servo control here. I(maciej) added code to print the 
-	// value of the z button to show you how to get it.
-    
+#endif
+	
+}
+
+void RobotBeta1::updatePanTilt()
+{
 	static float lastRightZ = 0;
 	static float lastLeftZ = 0;
 	float leftZ, rightZ;
@@ -375,14 +346,51 @@ void RobotBeta1::actOnButtons(void)
 		DBG("rightZ=%f leftZ=%f\n", leftZ, rightZ);
 		lastLeftZ = leftZ;
 		lastRightZ = rightZ;
-		// Eddy: set servo pwm here.
-			 
 		pan->Set((leftZ + 1.0) / 2.0);
 		tilt->Set((rightZ + 1.0) / 2.0);
-			 	 
 	}
 }
-	
 
+void RobotBeta1::actOnButtons(void)
+{
+	readButtons(stickLeft, leftButtons, "left");
+	readButtons(stickCopilot, copilotButtons, "copilot");
+	readButtons(stickRight, rightButtons, "right");
+	now = time(NULL);
+	if (now - lastTime >= 1) {
+		lastTime = now;
+		updateConveyor();
+	}
+	
+	updateShooter();
+	updatePanTilt();
+}
+
+void RobotBeta1::UpdateDashboard(void) 
+{
+	GetWatchdog().Feed();
+	dashboard->m_PWMChannels[0][0] = leftMotor->GetRaw();
+	dashboard->m_PWMChannels[0][1] = rightMotor->GetRaw();
+	dashboard->m_PWMChannels[0][2] = shooter->GetRaw();
+	dashboard->m_PWMChannels[0][3] = conveyor->GetRaw();
+	dashboard->m_PWMChannels[0][8] = pan->GetRaw();
+	dashboard->m_PWMChannels[0][9] = tilt->GetRaw();
+	//dashboard->m_DIOChannels[1] = encoder->GetRaw();
+	//dashboard->m_DIOChannels[2] = encoder->GetRaw();
+
+	DBG("\rPWM 1-%d 2-%d 3-%d 4-%d 8-%d 9-%d %d %d now=%d last=%d",
+		dashboard->m_PWMChannels[0][0],
+		dashboard->m_PWMChannels[0][1],
+		dashboard->m_PWMChannels[0][2],
+		dashboard->m_PWMChannels[0][3],
+		dashboard->m_PWMChannels[0][8],
+		dashboard->m_PWMChannels[0][9],
+		dashboard->m_DIOChannels[1],
+	    dashboard->m_DIOChannels[2],
+	    (int)now, (int)lastTime);
+	
+	// Call this last to send data to dashboard.
+    dashboard->PackAndSend();
+}
 
 START_ROBOT_CLASS(RobotBeta1);
