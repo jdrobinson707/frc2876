fec2039d450b00dca2948a991ddcc35afc175142
loafdog@gmail.com <loafdog@gmail.com@0d94b9bc-da79-11dd-9e03-45b345501c35>
2013-01-30 04:14:49 +0000

    Maciej: Copy vision sample 2013 code into bball robot

--- trunk/2013/Bball2013/src/org/usfirst/frc2876/Bball2013/subsystems/CameraTarget.java
+++ trunk/2013/Bball2013/src/org/usfirst/frc2876/Bball2013/subsystems/CameraTarget.java
@@ -20,9 +20,6 @@ import edu.wpi.first.wpilibj.image.CriteriaCollection;
 import edu.wpi.first.wpilibj.image.NIVision;
 import edu.wpi.first.wpilibj.image.NIVisionException;
 import edu.wpi.first.wpilibj.image.ParticleAnalysisReport;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
-import org.usfirst.frc2876.Bball2013.RobotMap;
 import org.usfirst.frc2876.Bball2013.Target;
 import org.usfirst.frc2876.Bball2013.commands.*;
 
@@ -46,53 +43,17 @@ public class CameraTarget extends Subsystem {
 
 
     }
-    public static final String[] pos = {"TOP", "RGT", "BOT", "LFT"};
-    public static final int TOP = 0;
-    public static final int RIGHT = 1;
-    public static final int BOT = 2;
-    public static final int LEFT = 3;
-    String angleHistory;
-    // bhs vals
-//    int rlow = 50;
-//    int glow = 43;
-//    int blow = 47;
     int rlow = 70;
     int glow = 40;
     int blow = 25;
     int ghigh = 255;
     int rhigh = 255;
     int bhigh = 255;
-// This worked at 7pm on a sunny day (lights off)
-//    int rlow = 10;
-//    int glow = 40;
-//    int blow = 25;
-//    int ghigh = 120;
-//    int rhigh = 255;
-////    int bhigh = 255;
-    // arena vals
-//    int rlow = 103;
-//    int rhigh = 255;
-//    int glow = 150;
-//    int ghigh = 255;
-//    int blow = 26;
-//    int bhigh = 255;
-    // practice area
-//    int rlow = 60;
-//    int rhigh = 255;
-//    int glow = 80;
-//    int ghigh = 255;
-//    int blow = 28;
-//    int bhigh = 255;
     int VIEW_ANGLE = 54;
     AxisCamera camera;
     CriteriaCollection cc;
     Preferences pfs = Preferences.getInstance();
     boolean filter_running = false;
-    Target[] sorted;
-    Target none;
-    double current_turn = 0;
-    double search = 1;
-    double search_incr = 0;
 
     public CameraTarget() {
         //super("CameraTarget");
@@ -100,8 +61,7 @@ public class CameraTarget extends Subsystem {
         camera = AxisCamera.getInstance();
 
         cc = new CriteriaCollection();
-        cc.addCriteria(NIVision.MeasurementType.IMAQ_MT_BOUNDING_RECT_WIDTH, 30, 400, false);
-        cc.addCriteria(NIVision.MeasurementType.IMAQ_MT_BOUNDING_RECT_HEIGHT, 40, 400, false);
+        cc.addCriteria(NIVision.MeasurementType.IMAQ_MT_AREA, 500, 65535, false);
 
         pfs.putInt("rlow", rlow);
         pfs.putInt("rhigh", rhigh);
@@ -115,99 +75,27 @@ public class CameraTarget extends Subsystem {
                 + " G=" + glow + "," + ghigh
                 + " B=" + blow + "," + bhigh);
 
-        none = new Target(null, 0);
-        resetTargets();
-        resetLastAngle();
     }
 
+    private void analyzeImage(BinaryImage threshImage, BinaryImage filterImage) {
 
-    public void resetLastAngle() {
-        angleHistory = "";
-    }
-
-    public void addLastAngle(double degrees) {
-        angleHistory += ":" + degrees;
-    }
-
-    public void printTargets() {
-        double pdelay = .2;
-        for (int i = 0; i < sorted.length; i++) {
-            System.out.println(pos[i] + ": " + sorted[i]);
-            Timer.delay(pdelay);
-        }
-    }
-
-    private void sort2(Target[] targets) {
-        if (targets == null || sorted == null) {
-            return;
-        }
-        if (targets.length == 0) {
-            return;
-        }
-        sorted[TOP] = targets[0];
-        for (int i = 0; i < targets.length; i++) {
-            if (targets[i].getYCoM() < sorted[TOP].getYCoM() || sorted[TOP] == none) {
-                sorted[TOP] = targets[i];
-            }
-        }
-        sorted[LEFT] = none;
-        for (int i = 0; i < targets.length; i++) {
-            if (targets[i].getXCoM() < sorted[LEFT].getXCoM() || sorted[LEFT] == none) {
-                sorted[LEFT] = targets[i];
-            }
-        }
-        sorted[RIGHT] = none;
-        for (int i = 0; i < targets.length; i++) {
-            if (targets[i].getXCoM() > sorted[RIGHT].getXCoM() || sorted[RIGHT] == none) {
-                sorted[RIGHT] = targets[i];
-            }
-        }
-        sorted[BOT] = none;
-        for (int i = 0; i < targets.length; i++) {
-            if (targets[i].getYCoM() > sorted[BOT].getYCoM() || sorted[BOT] == none) {
-                sorted[BOT] = targets[i];
-            }
-        }
-
-    }
-
-    private void analyzeImage(BinaryImage img) {
-        Target[] targets = null;
         try {
-            ParticleAnalysisReport[] reports = img.getOrderedParticleAnalysisReports();
-            System.out.println("num particles " + reports.length);
-            targets = new Target[4];
+            Target[] targets = new Target[filterImage.getNumberParticles()];
+            System.out.println("num particles " + targets.length);
             for (int i = 0; i < targets.length; i++) {
-                targets[i] = none;
-            }
-            Target[] tmp = new Target[reports.length];
-            int valid = 0;
-            for (int i = 0; i < reports.length; i++) {
-                Target t = new Target(reports[i], VIEW_ANGLE);
-                t.analyze();
-                if (t.isValid()) {
-                    tmp[i] = t;
-                    valid++;
+                targets[i] = new Target(filterImage.getParticleAnalysisReport(i), i,
+                        threshImage, filterImage);
+                targets[i].calcScores();
+                if (targets[i].isValid(true)) {
+                    targets[i].computeDistance(true);
+                    System.out.println("Outer " + targets[i]);
+                } else if (targets[i].isValid(false)) {
+                    targets[i].computeDistance(false);
+                    System.out.println("Center " + targets[i]);
                 } else {
-                    System.out.println("invalid: " + t);
-                    tmp[i] = none;
+                    System.out.println("particle " + i + " is not valid");
                 }
             }
-            targets = new Target[valid];
-            for (int i = 0, j = 0; i < tmp.length && j < valid; i++) {
-                if (tmp[i] != none) {
-                    targets[j] = tmp[i];
-                    j++;
-                }
-            }
-            sorted = new Target[4];
-            for (int i = 0; i < sorted.length; i++) {
-                sorted[i] = none;
-            }
-
-            sort2(targets);
-            current_turn = getBestTarget().getTurnDegrees();
-
         } catch (NIVisionException ex) {
             ex.printStackTrace();
         }
@@ -247,7 +135,6 @@ public class CameraTarget extends Subsystem {
     public void findTargets(boolean saveImage) {
         filter_running = true;
         try {
-            resetTargets();
             if (camera.freshImage() == false) {
                 filter_running = false;
                 return;
@@ -275,15 +162,13 @@ public class CameraTarget extends Subsystem {
             }
             if (saveImage && img_ctr < MAX_SAVE && img_ctr < 5) {
                 thresholdImage.write("/tmp/" + img_ctr + "1thresh-image.png");
-            } // remove small artifacts
-            BinaryImage bigObjectsImage = thresholdImage.removeSmallObjects(false, 1);
-            if (saveImage && img_ctr < MAX_SAVE) {
-                bigObjectsImage.write("/tmp/" + img_ctr + "2big-image.png");
-            } // fill in occluded rectangles
-            BinaryImage convexHullImage = bigObjectsImage.convexHull(false);
+            }
+            // fill in occluded rectangles
+            BinaryImage convexHullImage = thresholdImage.convexHull(false);
             if (saveImage && img_ctr < MAX_SAVE) {
                 convexHullImage.write("/tmp/" + img_ctr + "3convex-image.png");
-            } // find filled in rectangles
+            }
+            // find filled in rectangles
             BinaryImage filteredImage = convexHullImage.particleFilter(cc);
             if (saveImage && img_ctr < MAX_SAVE) {
                 filteredImage.write("/tmp/" + img_ctr + "4filter-image.png");
@@ -292,11 +177,10 @@ public class CameraTarget extends Subsystem {
                 printParticleReports(filteredImage);
             }
             //img_ctr++;
-            analyzeImage(filteredImage);
+            analyzeImage(thresholdImage, filteredImage);
 
             filteredImage.free();
             convexHullImage.free();
-            bigObjectsImage.free();
             thresholdImage.free();
             image.free();
         } catch (AxisCameraException ex) {
@@ -307,56 +191,4 @@ public class CameraTarget extends Subsystem {
         filter_running = false;
     }
 
-    // Get the first available target. If target is not found during image
-    // analysis the sorted array gets set to 'none' Target object which
-    // returns default values (probably zero) for distance and turn amount.
-    private Target getBestTarget() {
-        Target t = none;
-        if (sorted != null) {
-            if (sorted[TOP] != none) {
-                t = sorted[TOP];
-            } else if (sorted[BOT] != none) {
-                t = sorted[BOT];
-            } else if (sorted[LEFT] != none) {
-                t = sorted[LEFT];
-            } else if (sorted[RIGHT] != none) {
-                t = sorted[RIGHT];
-            }
-        }
-        return t;
-    }
-
-    public void resetTargets() {
-        if (sorted == null) {
-            sorted = new Target[4];
-        }
-        for (int i = 0; i < sorted.length; i++) {
-            sorted[i] = none;
-        }
-    }
-
-    public double getTurnAmount() {
-        Target t = getBestTarget();
-        if (t == none) {
-            current_turn = 0;
-//            search_incr++;
-//            if (search > 0) {
-//                search = 25 * search_incr * -1;
-//            } else {
-//                search = 25 * search_incr;
-//            }
-//            if (search > 180) {
-//                search = 0;
-//            }
-//            current_turn = search;
-        } else {
-            search_incr = 0;
-            current_turn = getBestTarget().getTurnDegrees();
-        }
-        return current_turn;
-    }
-
-    public double getDistance() {
-        return getBestTarget().getDistance();
-    }
 }
