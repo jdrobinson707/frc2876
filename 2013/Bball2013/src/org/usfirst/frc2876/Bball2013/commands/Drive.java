0e0116f7869d41179503c00572cfeedc0ca2eedc
loafdog <loafdog@0d94b9bc-da79-11dd-9e03-45b345501c35>
2013-02-02 21:47:45 +0000

    harsh - can adjust turning based on vision :)

--- 2013/Bball2013/src/org/usfirst/frc2876/Bball2013/commands/Drive.java
+++ 2013/Bball2013/src/org/usfirst/frc2876/Bball2013/commands/Drive.java
@@ -30,7 +30,7 @@ public class  Drive extends Command {
     }
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
     // Called once after isFinished returns true
     protected void end() {
@@ -38,5 +38,6 @@ public class  Drive extends Command {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+        end(); //
     }
 }
