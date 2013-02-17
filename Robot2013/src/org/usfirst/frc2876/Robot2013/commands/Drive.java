a909ed734b1caac349d4c54e2f2148c09f5c1602
loafdog@gmail.com <loafdog@gmail.com@0d94b9bc-da79-11dd-9e03-45b345501c35>
2013-02-17 18:39:36 +0000

    -Eric: -So far I've tried getting the encoders to work, but about 45 minutes ago Live Window would not change to Test Mode when I went into Test Mode on the driver station. The Bball2013 code works fine, and Live Window changes with it, but not the new 2013 code.

--- Robot2013/src/org/usfirst/frc2876/Robot2013/commands/Drive.java
+++ Robot2013/src/org/usfirst/frc2876/Robot2013/commands/Drive.java
@@ -22,12 +22,16 @@ public class  Drive extends Command {
     }
     // Called just before this Command runs the first time
     protected void initialize() {
-        //Robot.driveTrain.startEncoder(Robot.driveTrain.rightEncoder);
-        //Robot.driveTrain.startEncoder(Robot.driveTrain.leftEncoder);
+        Robot.driveTrain.startEncoder(Robot.driveTrain.rightEncoder);
+        Robot.driveTrain.startEncoder(Robot.driveTrain.leftEncoder);
     }
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
         Robot.driveTrain.drive(Robot.oi.getJoystick1(), Robot.oi.getJoystick2());
+        Robot.driveTrain.getLeftEncoder();
+        Robot.driveTrain.getRightEncoder();
+        Robot.driveTrain.getLeftEncoderDistance();
+        Robot.driveTrain.getRightEncoderDistance();
     }
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
