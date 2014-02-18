f1f47dfc1fd12e6034059066ebc9d39b87288091
team2876software <team2876software@0d94b9bc-da79-11dd-9e03-45b345501c35>
2014-02-18 12:26:25 +0000

    Amit, Salina, Ashay, Dipi, Hursh, Shamu, Imraan Re-commiting the code from the final Saturday of build season.

--- trunk/2014/FRC2876AerialAssault/src/org/usfirst/frc2876/FRC2876AerialAssault/XboxController.java
+++ trunk/2014/FRC2876AerialAssault/src/org/usfirst/frc2876/FRC2876AerialAssault/XboxController.java
@@ -1,84 +1,109 @@
 //http://www.chiefdelphi.com/forums/showthread.php?t=110777
-
 package org.usfirst.frc2876.FRC2876AerialAssault;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import org.usfirst.frc2876.FRC2876AerialAssault.commands.CollectAndRaise;
+import org.usfirst.frc2876.FRC2876AerialAssault.commands.LowerArm;
+import org.usfirst.frc2876.FRC2876AerialAssault.commands.MoveToScorePosition;
+import org.usfirst.frc2876.FRC2876AerialAssault.commands.RaiseArm;
+import org.usfirst.frc2876.FRC2876AerialAssault.commands.ReleaseBall;
 
 /**
  *
  * @author mentor
  */
 public class XboxController {
+
     private Joystick m_pad;
-    
+    // Creates buttons
+    public Button X;
+    public Button Y;
+    public Button A;
+    public Button B;
+    public Button lBumper;
+    public Button rBumper;
+    public Button start;
+    public Button back;
+    public Button lStick;
+    public Button rStick;
+    public Button lTrigger;
+    public Button rTrigger;
+    //public Button dLeft;
+
     public XboxController(int port) {
         m_pad = new Joystick(port);
+        // Creates buttons
+        X = new JoystickButton(m_pad, BUTTON_X);
+        Y = new JoystickButton(m_pad, BUTTON_Y);
+        A = new JoystickButton(m_pad, BUTTON_A);
+        B = new JoystickButton(m_pad, BUTTON_B);
+        lBumper = new JoystickButton(m_pad, BUMPER_L);
+        rBumper = new JoystickButton(m_pad, BUMPER_R);
+        start = new JoystickButton(m_pad, BUTTON_START);
+        back = new JoystickButton(m_pad, BUTTON_BACK);
+        lStick = new JoystickButton(m_pad, LEFT_STICK_PRESS);
+        rStick = new JoystickButton(m_pad, RIGHT_STICK_PRESS);
+        lTrigger = new JoystickButton(m_pad, L_TRIGGER_VAL);
+        rTrigger = new JoystickButton(m_pad, R_TRIGGER_VAL);
+    //public Button dLeft = new JoystickButton (m_pad, DPAD_LR);
+
+        Y.whenPressed(new RaiseArm());
+        A.whenPressed(new LowerArm());
+        X.whenPressed(new MoveToScorePosition());
+        B.whenPressed(new MoveToScorePosition());
+        //lBumper.whenPressed(new CollectBall());
+        lBumper.whenPressed(new CollectAndRaise());
+        rBumper.whenPressed(new ReleaseBall());
     }
-    
+
     public double getLeftX() {
         return m_pad.getRawAxis(LEFT_X_AXIS);
     }
-    
+
     public double getLeftY() {
         return m_pad.getRawAxis(LEFT_Y_AXIS);
     }
-    
+
     public double getRightX() {
         return m_pad.getRawAxis(RIGHT_X_AXIS);
     }
-    
+
     public double getRightY() {
         return m_pad.getRawAxis(RIGHT_Y_AXIS);
     }
-    
+
     public double getTriggers() {
         return m_pad.getRawAxis(TRIGGERS);
     }
-    
+
     public double getDpadX() {
         return m_pad.getRawAxis(DPAD_LR);
     }
-             
+
     public double applyDeadband(int axis) {
-        if(Math.abs(m_pad.getRawAxis(axis)) < .1) {
+        if (Math.abs(m_pad.getRawAxis(axis)) < .1) {
             return 0;
         } else {
             return axis;
         }
-    }          
-    
-    // Creates buttons
-    public Button X = new JoystickButton(m_pad, BUTTON_X);
-    public Button Y = new JoystickButton(m_pad, BUTTON_Y);
-    public Button A = new JoystickButton(m_pad, BUTTON_A);
-    public Button B = new JoystickButton(m_pad, BUTTON_B);
-    public Button lBumper = new JoystickButton(m_pad, BUMPER_L);
-    public Button rBumper = new JoystickButton(m_pad, BUMPER_R);
-    public Button start = new JoystickButton(m_pad, BUTTON_START);
-    public Button back = new JoystickButton(m_pad, BUTTON_BACK);
-    public Button lStick = new JoystickButton(m_pad, LEFT_STICK_PRESS);
-    public Button rStick = new JoystickButton(m_pad, RIGHT_STICK_PRESS);
-    public Button lTrigger = new JoystickButton (m_pad, L_TRIGGER_VAL);
-    public Button rTrigger = new JoystickButton (m_pad, R_TRIGGER_VAL);
-    
+    }
+
     public boolean getButton(int btn) {
         return m_pad.getRawButton(btn);
     }
-    
+
     // Axis indexes:
-    public static final int
-            LEFT_X_AXIS = 1,
+    public static final int LEFT_X_AXIS = 1,
             LEFT_Y_AXIS = 2,
             TRIGGERS = 3,
             RIGHT_X_AXIS = 4,
             RIGHT_Y_AXIS = 5,
             DPAD_LR = 6;
-    
+
     // Button mappings:
-    public static final int
-            BUTTON_A = 1,
+    public static final int BUTTON_A = 1,
             BUTTON_B = 2,
             BUTTON_X = 3,
             BUTTON_Y = 4,
@@ -90,5 +115,5 @@ public class XboxController {
             RIGHT_STICK_PRESS = 10,
             L_TRIGGER_VAL = 11,
             R_TRIGGER_VAL = 12;
-    
+
 }
